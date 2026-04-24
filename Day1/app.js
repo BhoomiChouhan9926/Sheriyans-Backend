cfe637e987dc0b181de15d506331c4bd2167e959
BhoomiChouhan@2004 <bhoomichouhan040@gmail.com>
2026-04-24 17:24:02 +0530

    day -7| done

---
+++ Day1/app.js
@@ -0,0 +1,3 @@
+const express = require('express')
+ const app = express() // this will create the server ans the instance of the server will be stored in the app
+app.listen(3000); // it will satrt the server at the port 3000
